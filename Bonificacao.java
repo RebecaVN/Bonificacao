a6df910d2079d8ee1399371fb977a99cf06add53
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-12 10:43:04 -0300

    Add files via upload

---
+++ Bonificacao.java
@@ -0,0 +1,43 @@
+package EstruturaDeDecisaoLogica;
+
+public class Bonificacao {
+	public static void main(String[] args) {
+		
+		
+		String nome1 = "Paula Alves";
+		double salario1 = 1356.25;
+		int tempoDeCasa1 = 1;
+		boolean atraso1 = false;
+		boolean falta1 = false;
+		
+		
+		String nome2 = "Alana Martins";
+		double salario2 = 2531.33;
+		int tempoDeCasa2 = 1;
+		boolean atraso2 = true;
+		boolean falta2 = false;
+		
+
+		String nome3 = "Gabriel Souza";
+		double salario3 = 1532.00;
+		int tempoDeCasa3 = 2;
+		boolean atraso3 = false;
+		boolean falta3 = false;
+		
+		
+		String nome4 = "Bruna Gomes";
+		double salario4 = 1354.33;
+		int tempoDeCasa4 = 2;
+		boolean atraso4 = false;
+		boolean falta4 = true;
+		
+		
+		String nome5 = "Jéssica Alves";
+		
+		
+		
+		
+		
+		
+	}
+}
